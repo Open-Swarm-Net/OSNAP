e8d2cb1ecc7b3d3f7bef5f61c81db4658057cb77
Dnapert <dannapert@gmail.com>
2023-05-02 16:13:15 -0400

    adding start script, blank react app

---
+++ ui/demo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
