ab5404a390319ceae1a3fea0c39fa089c03baad1
Dnapert <dannapert@gmail.com>
2023-05-03 14:12:37 -0400

    ui updates

--- ui/demo_snap/tailwind.config.js
+++ ui/demo_snap/tailwind.config.js
@@ -5,10 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   daisyui: {
-    themes: ["bumblebee"],
+    themes: [
+      {
+        mytheme: {
+        
+          "primary": "#a78bfa",
+          "secondary": "#9333ea",
+          "accent": "#6b7280",
+          "neutral": "#292524",
+          "base-100": "#171212",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
   },
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [require("daisyui")],
 }
