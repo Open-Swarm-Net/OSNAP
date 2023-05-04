e5cd9fb0fd9eb0183298e603a54fbeba3da38d1e
Dnapert <dannapert@gmail.com>
2023-05-04 13:44:26 -0400

    websocket hook in ui

--- ui/demo_snap/src/App.tsx
+++ ui/demo_snap/src/App.tsx
@@ -1,67 +1,46 @@
-import React from 'react';
+import React from 'react'
 import { LoadingSpinner } from './components/loadingSpinner'
-type Agent = {
-  name: string,
-  id: string,
-  tools: string[],
-  description: string,
-  scope: string,
-}
+import { useChatWebsocket, sendMessage } from './components/useChatWebSocket'
+import useFetchAgents from './components/useFetchAgents'
+
+
+const wsURL = `ws://localhost:8000/ws/${Date.now()}`
 
 function App() {
 
-  const [loading, setLoading] = React.useState(false)
-  const [agents, setAgents] = React.useState<Agent[]>([])
-  const controller = new AbortController()
-  const signal = controller.signal
-
   const urls = ['http://localhost:8000/agents', 'http://localhost:8005/agents']
 
-  const fetchAgents = async (signal: AbortSignal) => {
-    setLoading(true)
-    Promise.all(
-      urls.map(url => fetch(url, { signal })))
-      .then(responses => Promise.all(responses.map(res => res.json())))
-      .then(data => {
-        setAgents(data.flat() as Agent[])
-        console.log(data.flat())
-
-        setTimeout(() => {//simulate longer loading time
-          setLoading(false)
-        }, 500)
-      }
-      )
-      .catch(err => {
-        setLoading(false)
-        console.log(err)
-      })
-  }
-
-  React.useEffect(() => {
-    fetchAgents(signal)
-
-    return () => {
-      controller.abort()
-      setAgents([])
-    }
-  }, [])
+  const [messages, setMessages] = React.useState<string>()
+  const textRef = React.useRef<HTMLInputElement>(null)
+  const { loading, agents } = useFetchAgents({ urls: urls })
+  const socket = useChatWebsocket(wsURL, setMessages)
 
   return (
     <div className="flex flex-col items-center bg-base min-h-screen text-primary">
       <div className="flex flex-col items-center justify-center w-full h-16 bg-neutral">
         <h1 className="text-2xl font-bold">OSNAP</h1>
-        <div className={`absolute top-1/3 w-1/3 h-1/3 bg-accent rounded-md ${loading ? 'flex flex-col justify-center' : null}`} >
-          {loading ? <LoadingSpinner /> : agents.map((agent: any) => {
-            return (
-              <div className='flex flex-col items-center justify-center'>
-                <pre>{agent.name}</pre>
-              </div>
-            )
-          })}
+        <div>
+          {socket.readyState === 1 ? 'connected' : 'not connected'}
         </div>
       </div>
+      <input ref={textRef} type="text" />
+      <button className='btn btn-outline btn-primary m-1' onClick={() => { sendMessage(socket, textRef.current?.value) }}>Send</button>
+      <div className='relative flex flex-col items-center'>
+        <div className=' top-1/3 w-[400px] m-1 bg-accent rounded-md flex justify-center'>
+          <p>{messages}</p>
+        </div>
+      </div>
+      <div className={`relative  bg-neutral w-4/5 rounded-md ${loading ? 'flex flex-col justify-center' : null}`} >
+        {loading ? <LoadingSpinner /> : agents?.map((agent: any) => {
+          return (
+            <div className='flex flex-col items-center justify-center'>
+              <pre>{agent.name}</pre>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- ui/demo_snap/src/components/loadingSpinner.tsx
+++ ui/demo_snap/src/components/loadingSpinner.tsx
@@ -2,7 +2,7 @@
 
 export const LoadingSpinner = () => {
     return (<div className="relative flex flex-col items-center justify-center">
-        <div className="radial-progress text-primary animate-spin" style={{ "--value": 70 }}></div>
+        <div className="radial-progress text-primary animate-spin " style={{ '--value': 70 }}></div>
     </div>
     )
 }
